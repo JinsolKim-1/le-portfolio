9f6e57d2aaeec9130e27362fd3337420944f8b17
JinsolKim-1 <atomicnick43@gmail.com>
2025-10-09 01:03:27 +0800

    updated social links

--- script.js
+++ script.js
@@ -18,4 +18,14 @@ window.addEventListener("load", () => {
   });
 });
 
+function sendMail() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const subject = encodeURIComponent(`Message from ${name}`);
+  const body = encodeURIComponent(`From: ${name} (${email})\n\n${message}`);
+
+  window.location.href = `mailto:jinsolkimain@gmail.com?subject=${subject}&body=${body}`;
+}
 
